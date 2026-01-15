daccb43689a7cba89890ca67bd90d4193073bd39
itsviseph <vish12jos2000@gmail.com>
2026-01-15 11:23:54 +0530

    Add password check script

---
+++ password_check.py
@@ -0,0 +1,7 @@
+import getpass
+
+user_name = input('Enter username - ')
+pass_word = getpass.getpass('Enter password - ')
+
+secret_password = len(pass_word) * '*'
+print(f'Hey {user_name}, your password {secret_password} is {len(pass_word)} characters long.')
