7ceb45c346c3a4bba5a0970d0be4490de8acc033
itsviseph <vish12jos2000@gmail.com>
2026-01-21 17:38:53 +0530

    update

--- password_check.py
+++ password_check.py
@@ -1,7 +1,9 @@
 import getpass
 
-user_name = input('Enter username - ')
-pass_word = getpass.getpass('Enter password - ')
+user_name = input("Enter username - ")
+pass_word = getpass.getpass("Enter password - ")
 
-secret_password = len(pass_word) * '*'
-print(f'Hey {user_name}, your password {secret_password} is {len(pass_word)} characters long.')
+secret_password = len(pass_word) * "*"
+print(
+    f"Hey {user_name}, your password {secret_password} is {len(pass_word)} characters long."
+)
